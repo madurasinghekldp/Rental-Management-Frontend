08b1303c8652361dd698f81d739e864b0857702c
dulan <madurasinghekldp@gmail.com>
2024-07-26 17:59:02 +0530

    load items

--- src/app/components/item-manage/item-manage.component.ts
+++ src/app/components/item-manage/item-manage.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../services/item.service';
 
 @Component({
   selector: 'app-item-manage',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,NgFor],
   templateUrl: './item-manage.component.html',
-  styleUrl: './item-manage.component.css'
+  styleUrl: './item-manage.component.css',
+  providers:[ItemService]
 })
-export class ItemManageComponent {
+export class ItemManageComponent implements OnInit {
+  
+
+  public itemList:any = [];
+
+  constructor(private itemService: ItemService){}
+
+  ngOnInit(): void {
+    this.loadItems();
+  }
+
+  loadItems(){
+    this.itemService.getAll().subscribe(res=>{
+      this.itemList = res;
+    });
+  }
 
 }
